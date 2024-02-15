6f8e3b63e70675c31403bc97a0358cddbf6c51d4
김병주 <atker14@gmail.com>
2024-02-15 19:53:21 +0900

    Spring run 지우기

--- src/main/java/faraway/util/restformat/RestFormatApplication.java
+++ src/main/java/faraway/util/restformat/RestFormatApplication.java
@@ -1,13 +1,9 @@
 package faraway.util.restformat;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class RestFormatApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestFormatApplication.class, args);
+
     }
 
 }
