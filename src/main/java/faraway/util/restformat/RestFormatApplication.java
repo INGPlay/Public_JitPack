20f5408ec3bc37c5fb9ddf9f752d828837c4c292
김병주 <atker14@gmail.com>
2024-02-15 20:09:17 +0900

    수정

--- src/main/java/faraway/util/restformat/RestFormatApplication.java
+++ src/main/java/faraway/util/restformat/RestFormatApplication.java
@@ -1,9 +1,13 @@
 package faraway.util.restformat;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RestFormatApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(RestFormatApplication.class, args);
     }
 
 }
